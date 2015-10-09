a9035c6cd387f6bcd197a70ae8e2b22adfc886b6
Mutassem Abu rijea <mutassem7@gmail.com>
2015-10-09 19:59:17 +0000

    updates gemfile

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
+  get 'courses/index'
+
+  get 'solute/hi'
+
+  get 'greeter/hi'
+
+  get 'greeter/hello'
+  get 'greeter/goodbye'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'courses#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
